864760eb1012ec03018b6615094a0cc3e581672c
siruliina <91068804+siruliina@users.noreply.github.com>
2023-03-22 10:01:03 +0200

    Changes made according to 3.11 and bug fixed

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -82,7 +82,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
+      .get('http://localhost:3001/api/persons')
       .then(response => setPersons(response.data))
   }, [])
   
