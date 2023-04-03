513e189b7356adec76a6c5914719354550d69b15
siruliina <91068804+siruliina@users.noreply.github.com>
2023-04-03 15:53:03 +0300

    Exercises done until 4.6

--- osa4/blogilista/tests/most_likes.test.js
+++ osa4/blogilista/tests/most_likes.test.js
@@ -1,6 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
+describe('most likes', () => {
   const listWithManyBlogs = [
     {
       _id: '5a422a851b54a676234d17f7',
@@ -36,8 +36,29 @@ describe('total likes', () => {
     }
   ]
 
-  test('when list has many blogs equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithManyBlogs)
-    expect(result).toBe(34)
+  test('when list has many blogs the favorite with most likes is', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(listWithManyBlogs[2])
+  })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog the favorite with most likes is', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('when list is empty there should be no favorite', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
   })
 })
