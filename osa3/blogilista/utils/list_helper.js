32500c68c4c06788c3e339b0ee01effbed04d31a
siruliina <91068804+siruliina@users.noreply.github.com>
2023-04-03 13:13:10 +0300

    Exercise 4.4 done

---
+++ osa3/blogilista/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.forEach(blog => {
+    likes += blog.likes
+  })
+  return likes
+}
+
+module.exports = {
+  dummy, totalLikes
+}
