3c70beed22f5da41734f1945c5fc12b763cb38c0
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-01-09 13:57:44 +0200

    Tehtävät tehty 5.4 asti

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -9,10 +10,25 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [message, setMessage] = useState(null)
+  const [type, setType] = useState(null)
+  
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -23,21 +39,68 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+
+      blogService.setToken(user.token)
+
       setUser(user)
+      setMessage(`logged in successfully with ${username}`)
+      setType('success')
+      setTimeout(() => {setMessage(null)}, 5000)
       setUsername('')
       setPassword('')
+
     } catch (exception) {
-      setErrorMessage('wrong credentials')
+      setMessage('wrong credentials')
+      setType('error')
       setTimeout(() => {
-        setErrorMessage(null)
+        setMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = async(event) => {
+    event.preventDefault()
+    console.log("logging out user:", user.username)
+    setMessage(`logged out successfully with ${user.username}`)
+    setType('success')
+    setTimeout(() => {setMessage(null)}, 5000)
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
+  const handleCreate = (event) => {
+    event.preventDefault()
+    console.log("creating a new blog:", title)
+
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url
+    }
+  
+    blogService
+      .createBlog(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+      setMessage(`a new blog ${title} by ${author} was created`)
+        setType('success')
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+      })
+    
+      setTimeout(() => {setMessage(null)}, 5000)
+
+      
   }
 
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={message} type={type} />
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -66,7 +129,41 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={message} type={type} />
       <p>{user.name} logged in</p>
+      <button onClick={handleLogout}>logout</button>
+      
+      <h2>create new blog</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          title
+            <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+            <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+            <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
