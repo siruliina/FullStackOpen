8a114c72e1d5304b902aade755b57d18d75548a8
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-01-24 18:51:38 +0200

    Tehtävä 6.2 tehty

--- osa6/unicafe-redux/src/main.jsx
+++ osa6/unicafe-redux/src/main.jsx
@@ -7,21 +7,42 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+
+  const handleClick = (type) => {
+    if (type == 'good') {
+      store.dispatch({
+        type: 'GOOD'
+      })
+    }
+
+    if (type == 'ok') {
+      store.dispatch({
+        type: 'OK'
+      })
+    }
+
+    if (type == 'bad') {
+      store.dispatch({
+        type: 'BAD'
+      })
+    }
+
+    if (type == 'zero') {
+      store.dispatch({
+        type: 'ZERO'
+      })
+    }
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => handleClick('good')}>good</button> 
+      <button onClick={() => handleClick('ok')}>ok</button> 
+      <button onClick={() => handleClick('bad')}>bad</button>
+      <button onClick={() => handleClick('zero')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
