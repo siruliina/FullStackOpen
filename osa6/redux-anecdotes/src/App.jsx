0a0c47eb13d584f16965f305a5a4d2b6cc9031e2
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-01-26 13:37:55 +0200

    Tehtävät tehty 6.5 asti

---
+++ osa6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,42 @@
+import { addVoteTo, addAnecdote } from './reducers/anecdoteReducer'
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(addVoteTo(id))
+  }
+
+  const addNew = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(addAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addNew}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
