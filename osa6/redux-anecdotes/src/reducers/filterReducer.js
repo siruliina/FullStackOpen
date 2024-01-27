b9e5fd0d13d97599cf2d0b6c87ad05283b3a3d31
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-01-27 20:01:03 +0200

    Tehtävä 6.10 tehty

--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,4 +1,8 @@
-const filterReducer = (state = '', action) => {
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ''
+
+/*const filterReducer = (state = '', action) => {
     switch (action.type) {
       case 'SET_FILTER':
         return action.payload
@@ -14,4 +18,18 @@ export const filterChange = filter => {
     }
 }
 
-export default filterReducer
+export default filterReducer */
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      console.log('state now: ', state)
+      return action.payload
+    }
+  }
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
