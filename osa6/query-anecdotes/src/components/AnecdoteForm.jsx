b3b940e6d40a3ec848930df706ee17b2020f135a
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-01-29 21:18:56 +0200

    6.21 tehty ilman optimointia

--- osa6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
 
-  const onCreate = (event) => {
+  const queryClient = useQueryClient()
+  
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  /*const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData({ queryKey: ['anecdotes'] })
+      queryClient.setQueryData({ queryKey: ['anecdotes'] }, anecdotes.concat(newAnecdote))
+    }
+  })*/
+
+  const onCreate = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
 }
 
   return (
