25bb161c37aa8bc2d85113965ef854815811de00
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-02-06 17:42:13 +0200

    Tehtävä 8.3 tehty

--- osa8/library-backend/library-backend.js
+++ osa8/library-backend/library-backend.js
@@ -92,10 +92,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -104,6 +112,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => {
+      return authors.map((author) => {
+        const authorBooks = books.filter((book) => book.author === author.name);
+        return {
+          ...author,
+          bookCount: authorBooks.length,
+        };
+      });
+    },
   },
 };
 
