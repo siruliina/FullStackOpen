9cd4a0c90300a217ee98c3620d3aea04db12f6a0
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-02-07 21:12:02 +0200

    Tehtävä 8.8 tehty

---
+++ osa8/library-frontend/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
