fd2f026773a59d2bbe4e9768981698a24aac2a29
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-02-11 23:13:57 +0200

    Tehtävä 9.9 tehty

--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 const app = express();
-app.use(express.json());
+import cors from 'cors';
 
-const PORT = 3000;
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
