f46ffb1c71822f1810f60d2dc57dbfc6d94e5938
siruliina <siru-liina.karlin@edu.turkuamk.fi>
2024-02-13 15:38:47 +0200

    Tehtävä 9.25 tehty

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(cors());
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
+  console.log('someone pinged here!');
   res.send('pong');
 });
 
